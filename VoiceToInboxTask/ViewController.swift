de88c4f852a6c5c5ce7a351c4475e22b7775cded
shuji ochi <ponpoko1968@gmail.com>
2019-12-07 18:36:20 +0900

    プロジェクトと関連ファイルを追加

---
+++ VoiceToInboxTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VoiceToInboxTask
+//
+//  Created by 越智修司 on 2019/12/07.
+//  Copyright © 2019 越智修司. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
